b714216b63abfa64d33895c1c71dde942c131729
Ernest Kirstein <glank314@gmail.com>
2022-03-31 03:34:17 -0400

    Implemented sqlite backed group

--- tests/group_builder.py
+++ tests/group_builder.py
@@ -53,7 +53,39 @@ def test_build_g1modg2():
   builder.build()
   print(len(builder))
 
+def test_decode_atomic_decomposition():
+  atomic = gb.AtomicDecomposition([cube.Move.UP, cube.Move.DOWN_2])
+  encoded = atomic.encode()
+  decoded = gb.AtomicDecomposition.decode(encoded)
+  assert decoded == atomic
+
+def test_sqlite_group():
+  group = gb.SqliteGroup(':memory:', 'g0modg1')
+  ident = cube.G0ModG1.ident()
+  assert ident not in group
+  group[ident] = gb.AtomicDecomposition([])
+  assert ident in group
+  assert group[ident] == gb.AtomicDecomposition([])
+  
+  # Test symmetries are in group without needing to add all of them
+  front = ident.copy()
+  front.do(cube.Move.FRONT)
+  group[front] = gb.AtomicDecomposition([cube.Move.FRONT])
+  assert front in group
+  back = ident.copy()
+  back.do(cube.Move.BACK)
+  back_atomic = gb.AtomicDecomposition([cube.Move.BACK])
+  assert back in group
+  assert group[back] == back_atomic
+
+  # Test other group methods
+  assert len(group) == 2
+  for key in group.keys():
+    assert key in [ident, front]
+
 def main(cmdline_params):
+  test_sqlite_group()
+  test_decode_atomic_decomposition()
   test_build_g0modg1()
   #test_build_g1modg2()
   pass
--- thistlethwaite/group_builder.py
+++ thistlethwaite/group_builder.py
@@ -1,5 +1,6 @@
 from . import cube 
 import numpy as np
+import sqlite3
 
 class AtomicDecomposition:
   def __init__(self, moves:list[cube.Move]):
@@ -15,11 +16,37 @@ class AtomicDecomposition:
     for move in self.moves:
       cp.do(move)
     return cp
+  def encode(self) -> bytes:
+    return bytes([int(m) for m in self.moves])
+  @staticmethod
+  def decode(data:bytes):
+    moves = [cube.Move(i) for i in data]
+    return AtomicDecomposition(moves)
+  def __eq__(self, other) -> bool:
+    if len(self.moves) != len(other.moves):
+      return False
+    return all(sm == om for sm, om in zip(self.moves, other.moves))
 
 class Group:
+  def __contains__(self, cb:cube.CubeLike) -> bool:
+    """ Returns true if the given cube is in the group. """
+    raise NotImplementedError()
+  def __getitem__(self, cb:cube.CubeLike) -> AtomicDecomposition:
+    """ Returns an atomic decomposition of the given element of the group. """
+    raise NotImplementedError()
+  def __setitem__(self, key:cube.CubeLike, value:AtomicDecomposition) -> None:
+    """ Sets the atomic decomposition for a given element. """
+    raise NotImplementedError()
+  def __len__(self) -> int:
+    """ Returns the number of elements in the group, mod symmetries. """
+    raise NotImplementedError()
+  def keys(self):
+    """ Iterates over all elements of the group, mod symmetries. """
+    raise NotImplementedError()
+
+class MemoryGroup(Group):
   def __init__(self):
     self._dict = dict[cube.CubeLike, AtomicDecomposition]()
-    self._debug = False
   def __contains__(self, cb:cube.CubeLike):
     for _, sym in cb.iter_symmetries():
       if sym in self._dict:
@@ -44,9 +71,70 @@ class Group:
   def keys(self):
     return self._dict.keys()
 
+class SqliteGroup(Group):
+  def __init__(self, filename, table, key_clazz=None):
+    self.filename = filename
+    self.table = table
+    self.con = sqlite3.connect(self.filename)
+    self.key_clazz = key_clazz 
+    self.con.execute(f'''
+      CREATE TABLE IF NOT EXISTS
+      {table} (
+        key BLOB PRIMARY KEY,
+        moves BLOB
+      )
+    ''')
+  def __contains__(self, cb:cube.CubeLike) -> bool:
+    keys = []
+    for _, sym in cb.iter_symmetries():
+      keys.append(sym.encode())
+    query = f'SELECT COUNT(*) FROM {self.table} WHERE '+\
+      ' OR '.join('key = ?' for _ in range(len(keys)))
+    cur = self.con.cursor()
+    cur.execute(query, tuple(keys))
+    count = cur.fetchall()[0][0]
+    return count > 0
+  def __getitem__(self, cb:cube.CubeLike) -> AtomicDecomposition:
+    keys = []
+    sym_idxs = []
+    for sym_idx, sym in cb.iter_symmetries():
+      keys.append(sym.encode())
+      sym_idxs.append(sym_idx)
+    query = f'SELECT key, moves FROM {self.table} WHERE ' + \
+      ' OR '.join('key = ?' for _ in range(len(keys))) + \
+      ' LIMIT 1'
+    cur = self.con.execute(query, tuple(keys))
+    rows = cur.fetchall()
+    if not rows:
+      raise KeyError(f'{cb} not in group')
+    key, atomic_encoded = rows[0]
+    sym_idx = sym_idxs[keys.index(key)]
+    atomic = AtomicDecomposition.decode(atomic_encoded) 
+    transform = cube.SYMMETRY_TRANSFORMS[sym_idx]
+    inv_transform = np.linalg.inv(transform)
+    # S_i(cb) = _dict[sym]
+    # cb = S_i^-1(_dict[sym])
+    return atomic.transformed(inv_transform)
+  def __setitem__(self, key:cube.CubeLike, value:AtomicDecomposition) -> None:
+    if self.key_clazz is None:
+      self.key_clazz = key.__class__
+    self.con.execute(
+      f'INSERT OR REPLACE INTO {self.table} VALUES (?,?)',
+      (key.encode(), value.encode())
+    )
+    self.con.commit()
+  def __len__(self) -> int:
+    cur = self.con.execute(f'SELECT COUNT(*) FROM {self.table}')
+    count = cur.fetchall()[0][0]
+    return count
+  def keys(self):
+    cur = self.con.cursor()
+    for row in cur.execute(f'SELECT key FROM {self.table}'):
+      yield self.key_clazz.decode(row[0])
+
 class GroupBuilder:
   def __init__(self, root:cube.CubeLike, available_moves:list[cube.Move] = None):
-    self.group = Group()
+    self.group = MemoryGroup()
     self.group[root] = AtomicDecomposition([])
     self.unexplored = set([root])
     if available_moves is None:
